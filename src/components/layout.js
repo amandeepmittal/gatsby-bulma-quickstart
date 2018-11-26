c2538139da7334c0f071b4a846a1e2b5d677852c
Aman Mittal <amandeepmittal@live.com>
2018-11-26 15:51:10 +0530

    add media, bulma starter site complete

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaCss3Alt, FaHtml5 } from 'react-icons/fa';
 
 import './style.scss';
 
@@ -15,43 +16,208 @@ const Layout = ({ children }) => (
 			<div className="container">
 				<div className="columns">
 					<div className="column">
-						<div className="notification is-primary">
-							<h1 class="title is-size-4">Bulma?</h1>
-							<p className="is-size-5">
-								Aliqua sint id ea mollit aliquip ex anim excepteur nulla nulla.
-								Cupidatat enim elit labore aliquip ad. In ea exercitation fugiat
-								elit pariatur qui mollit. Laborum proident irure quis ut.
-								Consectetur excepteur Lorem Lorem aute exercitation pariatur
-								velit. Voluptate enim cillum quis aliqua sit duis anim
-								voluptate.
-							</p>
-						</div>
+						<article className="media notification is-primary">
+							<figure className="media-left">
+								<span className="icon">
+									<FaCss3Alt size="fa-2x" />
+								</span>
+							</figure>
+							<div className="media-content">
+								<div className="content">
+									<h1 className="title is-size-4">Bulma?</h1>
+									<p className="is-size-5">
+										Aliqua sint id ea mollit aliquip ex anim excepteur nulla
+										nulla. Cupidatat enim elit labore aliquip ad. In ea
+										exercitation fugiat elit pariatur qui mollit. Laborum
+										proident irure quis ut. Consectetur excepteur Lorem Lorem
+										aute exercitation pariatur velit. Voluptate enim cillum quis
+										aliqua sit duis anim voluptate.
+									</p>
+								</div>
+							</div>
+						</article>
 					</div>
 					<div className="column">
-						<div className="notification is-info">
-							<h1 class="title is-size-4">Bulma?</h1>
-							<p className="is-size-5">
-								Aliqua sint id ea mollit aliquip ex anim excepteur nulla nulla.
-								Cupidatat enim elit labore aliquip ad. In ea exercitation fugiat
-								elit pariatur qui mollit. Laborum proident irure quis ut.
-								Consectetur excepteur Lorem Lorem aute exercitation pariatur
-								velit. Voluptate enim cillum quis aliqua sit duis anim
-								voluptate.
-							</p>
-						</div>
+						<article className="media notification is-warning">
+							<figure className="media-left">
+								<span className="icon">
+									<FaCss3Alt size="fa-2x" />
+								</span>
+							</figure>
+							<div className="media-content">
+								<div className="content">
+									<h1 className="title is-size-4">Bulma?</h1>
+									<p className="is-size-5">
+										Aliqua sint id ea mollit aliquip ex anim excepteur nulla
+										nulla. Cupidatat enim elit labore aliquip ad. In ea
+										exercitation fugiat elit pariatur qui mollit. Laborum
+										proident irure quis ut. Consectetur excepteur Lorem Lorem
+										aute exercitation pariatur velit. Voluptate enim cillum quis
+										aliqua sit duis anim voluptate.
+									</p>
+								</div>
+							</div>
+						</article>
 					</div>
 					<div className="column">
-						<div className="notification is-warning">
-							<h1 class="title is-size-4">Bulma?</h1>
-							<p className="is-size-5">
-								Aliqua sint id ea mollit aliquip ex anim excepteur nulla nulla.
-								Cupidatat enim elit labore aliquip ad. In ea exercitation fugiat
-								elit pariatur qui mollit. Laborum proident irure quis ut.
-								Consectetur excepteur Lorem Lorem aute exercitation pariatur
-								velit. Voluptate enim cillum quis aliqua sit duis anim
-								voluptate.
-							</p>
-						</div>
+						<article className="media notification is-info">
+							<figure className="media-left">
+								<span className="icon">
+									<FaCss3Alt size="fa-2x" />
+								</span>
+							</figure>
+							<div className="media-content">
+								<div className="content">
+									<h1 className="title is-size-4">Bulma?</h1>
+									<p className="is-size-5">
+										Aliqua sint id ea mollit aliquip ex anim excepteur nulla
+										nulla. Cupidatat enim elit labore aliquip ad. In ea
+										exercitation fugiat elit pariatur qui mollit. Laborum
+										proident irure quis ut. Consectetur excepteur Lorem Lorem
+										aute exercitation pariatur velit. Voluptate enim cillum quis
+										aliqua sit duis anim voluptate.
+									</p>
+								</div>
+							</div>
+						</article>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section className="section">
+			<div className="container">
+				<div className="columns is-multiline">
+					<div className="column is-one-third">
+						<article className="media">
+							<figure className="media-left">
+								<span className="icon is-medium">
+									<FaCss3Alt size="fa-2x" className="has-text-warning" />
+								</span>
+							</figure>
+							<div className="media-content">
+								<div className="content">
+									<h1 className="title is-size-4">Bulma?</h1>
+									<p className="subtitle is-size-5">
+										Aliqua sint id ea mollit aliquip ex anim excepteur nulla
+										nulla. Cupidatat enim elit labore aliquip ad. In ea
+										exercitation fugiat elit pariatur qui mollit. Laborum
+										proident irure quis ut. Consectetur excepteur Lorem Lorem
+										aute exercitation pariatur velit. Voluptate enim cillum quis
+										aliqua sit duis anim voluptate.
+									</p>
+								</div>
+							</div>
+						</article>
+					</div>
+					<div className="column is-one-third">
+						<article className="media">
+							<figure className="media-left">
+								<span className="icon is-medium">
+									<FaHtml5 size="fa-2x" className="has-text-danger" />
+								</span>
+							</figure>
+							<div className="media-content">
+								<div className="content">
+									<h1 className="title is-size-4">Bulma?</h1>
+									<p className="subtitleis-size-5">
+										Aliqua sint id ea mollit aliquip ex anim excepteur nulla
+										nulla. Cupidatat enim elit labore aliquip ad. In ea
+										exercitation fugiat elit pariatur qui mollit. Laborum
+										proident irure quis ut. Consectetur excepteur Lorem Lorem
+										aute exercitation pariatur velit. Voluptate enim cillum quis
+										aliqua sit duis anim voluptate.
+									</p>
+								</div>
+							</div>
+						</article>
+					</div>
+					<div className="column is-one-third">
+						<article className="media">
+							<figure className="media-left">
+								<span className="icon is-medium">
+									<FaCss3Alt size="fa-2x" className="has-text-warning" />
+								</span>
+							</figure>
+							<div className="media-content">
+								<div className="content">
+									<h1 className="title is-size-4">Bulma?</h1>
+									<p className="subtitle is-size-5">
+										Aliqua sint id ea mollit aliquip ex anim excepteur nulla
+										nulla. Cupidatat enim elit labore aliquip ad. In ea
+										exercitation fugiat elit pariatur qui mollit. Laborum
+										proident irure quis ut. Consectetur excepteur Lorem Lorem
+										aute exercitation pariatur velit. Voluptate enim cillum quis
+										aliqua sit duis anim voluptate.
+									</p>
+								</div>
+							</div>
+						</article>
+					</div>
+					<div className="column is-one-third">
+						<article className="media">
+							<figure className="media-left">
+								<span className="icon is-medium">
+									<FaHtml5 size="fa-2x" className="has-text-danger" />
+								</span>
+							</figure>
+							<div className="media-content">
+								<div className="content">
+									<h1 className="title is-size-4">Bulma?</h1>
+									<p className="subtitleis-size-5">
+										Aliqua sint id ea mollit aliquip ex anim excepteur nulla
+										nulla. Cupidatat enim elit labore aliquip ad. In ea
+										exercitation fugiat elit pariatur qui mollit. Laborum
+										proident irure quis ut. Consectetur excepteur Lorem Lorem
+										aute exercitation pariatur velit. Voluptate enim cillum quis
+										aliqua sit duis anim voluptate.
+									</p>
+								</div>
+							</div>
+						</article>
+					</div>
+					<div className="column is-one-third">
+						<article className="media">
+							<figure className="media-left">
+								<span className="icon is-medium">
+									<FaCss3Alt size="fa-2x" className="has-text-warning" />
+								</span>
+							</figure>
+							<div className="media-content">
+								<div className="content">
+									<h1 className="title is-size-4">Bulma?</h1>
+									<p className="subtitle is-size-5">
+										Aliqua sint id ea mollit aliquip ex anim excepteur nulla
+										nulla. Cupidatat enim elit labore aliquip ad. In ea
+										exercitation fugiat elit pariatur qui mollit. Laborum
+										proident irure quis ut. Consectetur excepteur Lorem Lorem
+										aute exercitation pariatur velit. Voluptate enim cillum quis
+										aliqua sit duis anim voluptate.
+									</p>
+								</div>
+							</div>
+						</article>
+					</div>
+					<div className="column is-one-third">
+						<article className="media">
+							<figure className="media-left">
+								<span className="icon is-medium">
+									<FaHtml5 size="fa-2x" className="has-text-danger" />
+								</span>
+							</figure>
+							<div className="media-content">
+								<div className="content">
+									<h1 className="title is-size-4">Bulma?</h1>
+									<p className="subtitleis-size-5">
+										Aliqua sint id ea mollit aliquip ex anim excepteur nulla
+										nulla. Cupidatat enim elit labore aliquip ad. In ea
+										exercitation fugiat elit pariatur qui mollit. Laborum
+										proident irure quis ut. Consectetur excepteur Lorem Lorem
+										aute exercitation pariatur velit. Voluptate enim cillum quis
+										aliqua sit duis anim voluptate.
+									</p>
+								</div>
+							</div>
+						</article>
 					</div>
 				</div>
 			</div>
